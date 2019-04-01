ac834d811e3dc4394eab478195d5bdc6546e3f38
Aronwebpro <aronwebpro@gmail.com>
2019-04-01 11:35:19 -0500

    D3 init

--- imports/ui/components/SkillsDiagram/index.js
+++ imports/ui/components/SkillsDiagram/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as d3 from 'd3';
 
 //Styles
 import './skills-diagram.css';
@@ -7,9 +8,42 @@ import './skills-diagram.css';
 export default class SkillsDiagram extends React.Component {
     render() {
         return (
-            <div>
+            <div className='skills-diagram-container'>
+                <div className='skills-diagram-canvas'>
 
+                </div>
             </div>
         );
     }
+
+    componentDidMount() {
+        this.drawChart();
+    }
+
+    drawChart = () => {
+        // const data = [{
+        //     title: 'JavaScript',
+        //     name: 'javascript',
+        //     value: 100,
+        // }];
+        // const svg = d3.select(DOM.svg(width, height))
+        //     .style("width", "100%")
+        //     .style("height", "auto")
+        //     .attr("font-size", 10)
+        //     .attr("font-family", "sans-serif")
+        //     .attr("text-anchor", "middle");
+        const data = [20, 5, 6, 6, 9, 10];
+        const svg = d3.select(".skills-diagram-canvas").append("svg").attr("width", 700).attr("height", 300);
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * 70)
+            .attr("y", 0)
+            .attr("width", 50)
+            .attr("height", (d, i) => d * 10)
+            .attr("fill", "green");
+    }
+
+
 }
