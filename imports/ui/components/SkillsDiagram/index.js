a2e7ee98780c7d4ca06dd723815b8ceed3f3ee1a
Aronwebpro <aronwebpro@gmail.com>
2019-04-07 20:12:46 -0500

    Buble Diagram backbone

--- imports/ui/components/SkillsDiagram/index.js
+++ imports/ui/components/SkillsDiagram/index.js
@@ -21,28 +21,97 @@ export default class SkillsDiagram extends React.Component {
     }
 
     drawChart = () => {
-        // const data = [{
-        //     title: 'JavaScript',
-        //     name: 'javascript',
-        //     value: 100,
-        // }];
-        // const svg = d3.select(DOM.svg(width, height))
-        //     .style("width", "100%")
-        //     .style("height", "auto")
-        //     .attr("font-size", 10)
-        //     .attr("font-family", "sans-serif")
-        //     .attr("text-anchor", "middle");
-        const data = [20, 5, 6, 6, 9, 10];
-        const svg = d3.select(".skills-diagram-canvas").append("svg").attr("width", 700).attr("height", 300);
-        svg.selectAll("rect")
-            .data(data)
+        const dataSet = {
+            children: [
+                {
+                    name: "JavaScript",
+                    size: 4319,
+                    color: 'red',
+                },
+                {
+                    name: "React Js",
+                    size: 3000,
+                    color: 'blue',
+                },
+                {
+                    name: "CSS",
+                    size: 1487,
+                    color: 'green',
+                },
+                {
+                    name: "HTML",
+                    size: 1636,
+                    color: '#ededed',
+                }
+            ]
+        };
+
+        const diameter = 600; //window.innerWidth - 50 ||  600;
+
+        //Draw SVG canvas
+        const svg = d3.select(".skills-diagram-canvas")
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+
+        const nodes = d3.hierarchy(dataSet)
+            .sum(function (d) {
+                return d.size;
+            });
+
+        const bubble = d3.pack(dataSet)
+            .size([diameter, diameter])
+            .padding(1.5);
+
+        const node = svg.selectAll(".node")
+            .data(bubble(nodes).descendants())
             .enter()
-            .append("rect")
-            .attr("x", (d, i) => i * 70)
-            .attr("y", 0)
-            .attr("width", 50)
-            .attr("height", (d, i) => d * 10)
-            .attr("fill", "green");
+            .filter((d) => {
+                return !d.children;
+            })
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", (d) => {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        //Append Circle Graphics to Node
+        node.append("circle")
+            .attr("r", (d) => {
+                return d.r;
+            })
+            .style("fill", (d, i) => {
+                return d.data.color;
+            });
+
+
+        //Append Titles of circle
+        node.append("text")
+            .attr("dy", ".2em")
+            .style("text-anchor", "middle")
+            .text(function (d) {
+                return d.data.name.substring(0, d.r / 3);
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", (d) => {
+                return d.r / 5;
+            })
+            .attr("fill", "white");
+
+        //Text of Number
+        node.append("text")
+            .attr("dy", "1.3em")
+            .style("text-anchor", "middle")
+            .text((d) => {
+                return d.data.size;
+            })
+            .attr("font-family", "Gill Sans", "Gill Sans MT")
+            .attr("font-size", function (d) {
+                return d.r / 5;
+            })
+            .attr("fill", "white");
+
     }
 
 
