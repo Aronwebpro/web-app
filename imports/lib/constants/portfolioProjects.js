f7a270dd45b555747ddc73ea05fdda51ffc09e69
Aronwebpro <aronwebpro@gmail.com>
2019-05-11 23:33:27 -0500

    Project page descriptions

--- imports/lib/constants/portfolioProjects.js
+++ imports/lib/constants/portfolioProjects.js
@@ -16,24 +16,29 @@ const portfolioProjects = [
                 render: () => (
                     <div>
                         <h3>Company: <strong>Vanee Foods</strong></h3>
-                        <h4><strong>Main Task:</strong> Join to working team, contribute to the project</h4>
+                        {/*<h4><strong>Main Task:</strong> Join to team, contribute to the project.</h4>*/}
+                        <h4><strong>Project Tasks:</strong></h4>
                         <ul>
-                            <h4><strong>General Tasks:</strong></h4>
                             <li>Users Management</li>
-                            <li>Inventory Management and tracking</li>
-                            <li>Lab Testing Management</li>
-                            <li>Tag Scanners Integration</li>
+                            <li>Inventory management and tracking</li>
+                            <li>Lab tests management</li>
+                            <li>Tag scanners integration</li>
                         </ul>
                         <div>
                             <p>
                                 The Vanee Foods Company manufactures shelf stable entrees, gravies, sauces, soups and soup bases.
                             </p>
                             <p>
-                                Our task to create complete internal inventory and ingredients automated management system, to track exact location
+                                Our task to create complete internal inventory and ingredients automated management system, to
+                                track exact location
                                 and state of all ingredients, which coming to the plant and coming out of it.
                             </p>
-                            <p>I'm proudly contribute to this project by adding new features and develop new tools. With my team mates we creating
-                                very sophisticated and advance systems to manage goods flow and manufacture process</p>
+                            <p>
+                                I've joined to this project in 2019. My contribution was to add new features and develop new
+                                tools. With my
+                                team we are creating very sophisticated and advance systems to manage goods and manufacture
+                                process.
+                            </p>
                         </div>
                     </div>
                 ),
@@ -50,8 +55,8 @@ const portfolioProjects = [
                     <div>
                         <h3>BidKid is Web Application for Painters</h3>
                         <h4><strong>Main Task:</strong> Connect ZOHO CRM Api with BidKId App</h4>
+                        <h4><strong>Other Tasks:</strong></h4>
                         <ul>
-                            <h4><strong>General Tasks:</strong></h4>
                             <li>
                                 Create REST API end points to trigger appropriate event
                             </li>
@@ -60,15 +65,15 @@ const portfolioProjects = [
                             </li>
                         </ul>
                         <div>
-                            <p>BidKid is estimate templating system, helping for painters connect with clients, create estimates and templates,
+                            <p>BidKid is estimate templating system, helping for painters connect with clients, create estimates
+                                and templates,
                                 schedule appointment and other functionalities. <a href="https://bidkid.io/">More about BidKid</a>
                             </p>
                             <p>
-                                ZOHO CRM is is an award-winning web-based CRM, <a href="https://www.zoho.com/crm/">More about ZOHO</a>
+                                ZOHO CRM is is an award-winning web-based CRM, <a href="https://www.zoho.com/crm/">More about
+                                ZOHO</a>
                             </p>
-                            <p>This was an interesting project to add new feature into existing application. The main challenge was to understand
-                                existing code and
-                                elegantly integrate new functionality</p>
+                            <p>This was an interesting project to implement new feature into existing application.</p>
                         </div>
                     </div>
                 )
@@ -89,7 +94,10 @@ const portfolioProjects = [
                 render: () => (
                     <div>
                         <h3>Central Parks is framework for Park Districts</h3>
-                        <h4><strong>Main Task:</strong> Create Admin Dashboard for Check-in game used by central Parks Mobile Apps</h4>
+                        <h4>
+                            <strong>Main Task:</strong> Create Admin Dashboard for Check-in game which is used by all Central
+                            Parks Mobile Apps.
+                        </h4>
                         <ul>
                             <h4><strong>General Tasks:</strong></h4>
                             <li>Create Admin Dashboard UI</li>
@@ -97,8 +105,9 @@ const portfolioProjects = [
                             <li>Users Management</li>
                         </ul>
                         <div>
-                            <p>Central Parks is framework which offers mobile application and admin dashboard for park districts.</p>
-                            <p>I had a honor to create mobile application framework and check-in game dashboard from scratch.</p>
+                            <p>Central Parks is framework which offers mobile application and admin dashboard for park
+                                districts.</p>
+                            <p>I had a honor to create mobile and check-in game management applications from scratch.</p>
                         </div>
                     </div>
                 )
@@ -113,11 +122,12 @@ const portfolioProjects = [
                 url: '',
                 render: () => (
                     <div>
-                        <h3>Wild Goose Chasers is pricate company offering protection of your property from wild goose</h3>
-                        <h4><strong>Main Task:</strong> Create Schedule and User management Dashboard, create Mobile application for company employees
+                        <h4>Wild Goose Chasers is private company offering protection of your property from wild goose</h4>
+                        <h4><strong>Main Task:</strong> Create Schedule and User management Dashboard, create Mobile application
+                            for company employees
                         </h4>
+                        <h4><strong>General Tasks:</strong></h4>
                         <ul>
-                            <h4><strong>General Tasks:</strong></h4>
                             <li>Users Management</li>
                             <li>REST API to communicate with mobile application</li>
                             <li>ZOHO CRM Integration</li>
@@ -127,6 +137,7 @@ const portfolioProjects = [
                         <div>
                             <p>Custom crafted business application design specifically for customer</p>
                             <p>Mobile application and Admin's dashboard was created from scratch by me.</p>
+                            <p>Mobile and web applications was created by me.</p>
                         </div>
                     </div>
                 )
@@ -152,7 +163,7 @@ const portfolioProjects = [
                             <li>User Management, Login with Google, Facebook or Email</li>
                         </ul>
                         <div>
-                            <p></p>
+                            <p>Another very interesting project built by me.</p>
                         </div>
                     </div>
                 )
@@ -169,7 +180,8 @@ const portfolioProjects = [
                 render: () => (
                     <div>
                         <h3>Vertabiz Social Application</h3>
-                        <h4><strong>Main Task:</strong> Create Mobile Application share pictures and thoughts with your coworkers</h4>
+                        <h4><strong>Main Task:</strong> Create Mobile Application share pictures and thoughts with your coworkers
+                        </h4>
                         <ul>
                             <h4><strong>General Tasks:</strong></h4>
                             <li>Mobile UI</li>
@@ -179,7 +191,7 @@ const portfolioProjects = [
                             <li>User Management</li>
                         </ul>
                         <div>
-                            <p></p>
+                            <p>Social application built by three team members. I mostly worked on applications UI.</p>
                         </div>
                     </div>
                 )
@@ -197,15 +209,17 @@ const portfolioProjects = [
                     <div>
                         <h3>Game Forum web application</h3>
                         <h4><strong>Main Task:</strong> Create games forum to create and share your posts, update or delete. </h4>
+                        <h4><strong>General Tasks:</strong></h4>
                         <ul>
-                            <h4><strong>General Tasks:</strong></h4>
                             <li>WEB UI and design</li>
                             <li>Firebase integration</li>
                             <li>Utilize React Js framework</li>
                             <li>User Management</li>
                         </ul>
                         <div>
-                            <p>This is my personal project where I wanted to implement new features of React Js, lear Firebase database features.</p>
+                            <p>This is my personal project where I wanted to implement new React Js feature and learn more about
+                                Firebase
+                                database.</p>
                         </div>
                     </div>
                 )
@@ -224,6 +238,23 @@ const portfolioProjects = [
                 appType: 'Website',
                 appTypeColor: { backgroundColor: '#429934' },
                 url: '',
+                render: () => (
+                    <div>
+                        <h3>Dealer eProcess makes websites for <br/>Auto Dealers</h3>
+                        <h4><strong>Main Task:</strong> Re-skin website pages using HTML, CSS, JavaScript.</h4>
+                        <h4><strong>General Tasks:</strong></h4>
+                        <ul>
+                            <li>Implement dealers' special UI orders</li>
+                            <li>Create new design features</li>
+                            <li>Update existing design</li>
+                            <li>Place banners</li>
+                        </ul>
+                        <div>
+                            <p>Dealer eProcess manages over thousand dealers websites where front-end team has to implement new
+                                features everyday.</p>
+                        </div>
+                    </div>
+                )
             },
             {
                 title: 'Dealer eProces Images Swipe',
@@ -234,6 +265,22 @@ const portfolioProjects = [
                 appType: 'Script',
                 appTypeColor: { backgroundColor: '#ae3738' },
                 url: '',
+                render: () => (
+                    <div>
+                        <h4><strong>Main Task:</strong> Create JavaScript code to allow image gallery listen finger swipe even and
+                            change pictures on swipe action.</h4>
+                        <h4><strong>General Tasks:</strong></h4>
+                        <ul>
+                            <li>Make integration in existing galleries code</li>
+                            <li>Prevent scroll on slide</li>
+                            <li>Implement excellent user experience</li>
+                        </ul>
+                        <div>
+                            <p>It's just simple script, but it wasn't easy to make an update into legacy huge code. Couple little
+                                trick made UX very smooth.</p>
+                        </div>
+                    </div>
+                )
             },
             {
                 title: 'PHP Auto Parts Search',
@@ -244,6 +291,21 @@ const portfolioProjects = [
                 appType: 'Web Application',
                 appTypeColor: { backgroundColor: '#fed656' },
                 url: '',
+                render: () => (
+                    <div>
+                        <h4>PHP Application allow user to search for specific car model and get all details about it. Included
+                            vehicle pictures, user reviews, specifications.</h4>
+                        <h4><strong>General Tasks:</strong></h4>
+                        <ul>
+                            <li>Create MVC model with PHP</li>
+                            <li>Using jQuery and AJAX request fetch data from Edmunds API</li>
+                            <li>Create application UI</li>
+                        </ul>
+                        <div>
+                            <p>Personal project to learn more about MVC and PHP</p>
+                        </div>
+                    </div>
+                )
             },
             {
                 title: 'Heathy Living Skills',
@@ -329,12 +391,26 @@ const portfolioProjects = [
             {
                 title: 'Ledinis.lt',
                 description: 'Website administration',
-                date: 'January 2015',
-                img: '/img/projects/terry-project.png',
+                date: 'June 2014 - January 2015',
+                img: '/img/projects/ledinis.png',
                 type: 'Work',
                 appType: 'Website',
                 appTypeColor: { backgroundColor: '#429934' },
                 url: '',
+                render: () => (
+                    <div>
+                        <h4>OpenCart CMS management</h4>
+                        <h4><strong>General Tasks:</strong></h4>
+                        <ul>
+                            <li>Upload pictures and descriptions</li>
+                            <li>Manage SEO</li>
+                            <li>Google AddWords and marketing campaigns</li>
+                        </ul>
+                        <div>
+                            <p>As a department manager I totally control our online store. This is where I started to get first web developing experience.</p>
+                        </div>
+                    </div>
+                )
             }
         ],
     },
