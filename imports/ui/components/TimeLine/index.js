cf548f0b7c3496236ede98bd44e89da93c37c555
Aronwebpro <aronwebpro@gmail.com>
2019-05-21 19:10:02 -0500

    portfolio page mobile 1

--- imports/ui/components/TimeLine/index.js
+++ imports/ui/components/TimeLine/index.js
@@ -4,48 +4,46 @@ import React from 'react';
 import './portfolio-timeline.css';
 
 //Components
-import Accordion from '/imports/ui/components/Accordion';
-
 
 class TimelineItem extends React.PureComponent {
     render() {
         const {
             title,
-            description,
             img,
             date,
-            projectId,
             type,
             appType,
             appTypeColor,
             render,
         } = this.props;
         return (
-            <div className='timeline-item'>
+            <div className='timeline-item-container'>
                 <div className='timeline-img'/>
-                <div className='timeline-card shadow'>
-                    <div>
-                        <div
-                            className='timeline-img-header'
-                            style={{ backgroundImage: `url(${img})` }}
-                        >
-                            <h2>{title}</h2>
+                <div className='timeline-item'>
+                    <div className='timeline-card shadow'>
+                        <div>
+                            <div
+                                className='timeline-img-header'
+                                style={{ backgroundImage: `url(${img})` }}
+                            >
+                                <h2>{title}</h2>
+                            </div>
+                            <div className='timeline-card-date'>
+                                <strong> {date}</strong>
+                            </div>
+                            <div className='project-type-container'>
+                                <h3><strong>#{type} project</strong></h3>
+                                <h3 style={appTypeColor || {}}>{appType}</h3>
+                            </div>
                         </div>
-                        <div className='timline-card-date'>
-                            <strong> {date}</strong>
+                        <div className='timeline-card-description'>
+                            {render && render()}
                         </div>
-                        <div className='project-type-container'>
-                            <h3><strong>#{type} project</strong></h3>
-                            <h3 style={appTypeColor || {}}>{appType}</h3>
+                        <div className='timeline-card-button-container'>
+                            <button className='btn'>
+                                More
+                            </button>
                         </div>
-                    </div>
-                    <div className='timline-card-description'>
-                        {render && render()}
-                    </div>
-                    <div className='timline-card-button-container'>
-                        <button className='btn'>
-                            More
-                        </button>
                     </div>
                 </div>
             </div>
