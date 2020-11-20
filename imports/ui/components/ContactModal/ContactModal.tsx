9f8e3db0ec6b73af17bf23a36e167a191d382072
Aronwebpro <aronwebpro@gmail.com>
2020-11-19 19:47:27 -0600

    CHORE Refactor component export file

--- imports/ui/components/ContactModal/ContactModal.tsx
+++ imports/ui/components/ContactModal/ContactModal.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import PropTypes = require('prop-types');
+import * as PropTypes from 'prop-types';
 
 // Components
 import Modal from 'imports/ui/components/Modal';
